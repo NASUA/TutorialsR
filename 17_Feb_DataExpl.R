23b552ee5dda64b4e548dabee8a9ccf8c5dbf610
Gabriel Muñoz <fgabriel1891@gmail.com>
2021-02-20 14:34:43 -0500

    Update 17_Feb_DataExpl.R

--- 17_Feb_DataExpl.R
+++ 17_Feb_DataExpl.R
@@ -102,9 +102,143 @@ sd(ObsYear$x)
 
 
 
-######
-# Manipulacion y arreglo de datos (END)
-######
+#### Feb 20
+
+# Review 
+# https://qcbsrworkshops.github.io/workshop05/pres-en/workshop05-pres-en.html#1
+# Objects
+
+c <- c(1,2,3,4)
+c2 <- c(T,F,T,F)
+
+myMatrix <- matrix(c(1,1,1,1,0,0,0,0,1,1,0,1,1,1,1), 4,5)
+
+myDF <- data.frame("Var1" = c(1:10),
+                   "Var2" = rep(c("a","b","c","d","e"),2),
+                   "Var3" = sample(c(1:100),10))
+
+myList <- list("c" = c,
+               "c2" = c2,
+               "Ambiente" = myMatrix, 
+               "Species" = myDF)
+
+str(myList)
+
+# Functions
+
+a <- 2
+b <- a+a
+
+c <- sqrt(2)
+
+library(vegan)
+
+resul <- vegan::decostand(c(1:30), "max")
+
+
+myFunction <- function(a,b,c){
+  g <- a+b
+  g <- g/c
+  g <- g^2
+  return(g)
+}
+
+
+plot(myFunction(1,4,5),
+     myFunction(1,4,5))
+
+
+## Control flow 
+
+
+## Decision making 
+## if , else, ifelse 
+r <- c(1:10)
+
+## Evalua 1 elemento
+if(r>5){
+  print("blue")
+}else{
+  print("red")
+}
+
+# Evalua sobre un vector
+ifelse(r>5,print("blue"), print("red"))
+
+# condiciones mas complejas 
+
+ifelse(r>5,
+       print("blue"), 
+       ifelse(r<3,
+              print("red"),
+              ifelse(r == 1,
+                     print("UNO"),r
+              )))
+
+if((2+2) == 4){
+  print("Arithmetic works.")
+}else {
+  print("Houston, we have a problem.")
+  
+}
+
+  
+
+a <- c(1:10,30:40)
+
+ifelse(a>10, "mayor 10", "menor10")
+c <- c()
+for(i in 1:length(a)){
+  
+  c[i] <- if(a[i]>10){print("mayor10")}
+  else{print("menor10")}
+  
+}
+
+
+
+for(i in 1:length(a)){
+  
+  c[i] <- a[i]^2
+  
+}
+
+
+### Apply family
+
+myMat <- matrix(c(1:10,21:30), 10,2 )
+
+apply(myMat,2,mean)
+
+b <- myMat[,1]
+
+
+c <-c()
+t1 <- Sys.time()
+for(i in 1:length(b)){
+  c[i] <- b[i]/sum(b)
+  
+}
+t2 <- Sys.time()
+t2-t1
+
+t3 <- Sys.time()
+sapply(1:length(b), function(x) b[x]/sum(b))
+t4 <- Sys.time()
+
+
+lapply(1:length(b), function(x) data.frame(b[x]/sum(b), c))
+
+#############
+## for loop
+## break and next 
+## repeat
+## while 
+
+### Exercise with own data 
+
+
+
 
 
 
